8380c1445d0b72ec482542a49a4915fe7722f64a
Chris Cirelli <ccirelli2@gmail.com>
2021-12-20 17:16:50 -0500

    Add directories, import ex data and how to set up the network graph.

--- examples/network_graph.py
+++ examples/network_graph.py
@@ -3,4 +3,31 @@ Examples of how to create network graphs in python.
 
 References:
     https://towardsdatascience.com/from-dataframe-to-network-graph-bbb35c8ab675
+    https://www.analyticsvidhya.com/blog/2018/04/introduction-to-graph-theory-network-analysis-python-codes/
 """
+# Libraries
+import os
+import pandas as pd
+import numpy
+import networkx as nx
+from decouple import config as d_config
+from matplotlib.pyplot import figure
+
+# Directories
+DIR_SRC = d_config('DIR_SRC')
+DIR_DATA = d_config('DIR_DATA')
+
+# Data Files
+JIRA_DATA_FILENAME = 'jira_sample.csv'
+JIRA_DATA = pd.read_csv(os.path.join(DIR_DATA, JIRA_DATA_FILENAME))
+JIRA_DATA_LIM = JIRA_DATA.loc[:, ['Assignee', 'Reporter']]
+
+# Create Instance of Graph
+nx_Graph = nx.Graph()
+
+# Populate Graph
+n_Graph = nx.from_pandas_edgelist(JIRA_DATA_LIM, 'Assignee', 'Reporter')
+
+# Create Figure
+figure(figsize=(10, 8))
+nx.draw_shell(n_Graph, with_labels=True)
